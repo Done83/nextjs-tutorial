c8e74759427e69a6a373b4bab35421686a9a4d78
Done83 <nedomehan@gmail.com>
2024-07-06 10:58:41 +0200

    Simple links completed

---
+++ app/about/page.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+const AboutPage = () => {
+  return (
+    <div>
+      <h1 className="text-7xl">AboutPage</h1>
+      <Link href='/'>Home Page</Link>
+    </div>
+  );
+};
+export default AboutPage;
